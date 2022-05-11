31012f06262d3f88d9bf8966cc23d86137d27485
Ivan <work.gorobec@gmail.com>
2022-05-11 21:58:25 +0500

    настроит store добавит в index

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
 import App from './components/app/app';
+import {configureStore} from '@reduxjs/toolkit';
+import {rootReducer} from './store/rootReducer';
+import {Provider} from 'react-redux';
+import {createApi} from './service/api';
+import 'react-toastify/dist/ReactToastify.css';
+import {ToastContainer} from 'react-toastify';
+import {onUnauthorizedError} from './service/const';
+
+const api = createApi(onUnauthorizedError);
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api
+      }
+    })
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+      <ToastContainer />
+    </Provider>
   </React.StrictMode>,
+
   document.getElementById('root')
 );
-
-reportWebVitals();

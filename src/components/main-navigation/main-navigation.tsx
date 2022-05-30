a8073a828dcb4f80c28f5bd747158779125708eb
Ivan <work.gorobec@gmail.com>
2022-05-30 20:48:06 +0500

    вынесет функцию в const, исправит href у ссылок

--- src/components/main-navigation/main-navigation.tsx
+++ src/components/main-navigation/main-navigation.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {useAppDispatch} from '../../hooks/useAppDispatch';
 import {useSelector} from 'react-redux';
 import {getCurrentFilter} from '../../store/filters-data/filters-data';
-import {AppFilter} from '../../service/const';
+import {AppFilter, getFilteredMovies} from '../../service/const';
 import {changeFilter} from '../../store/actions';
 import {getMoviesList} from '../../store/main-data/main-data';
 
@@ -13,39 +13,28 @@ function MainNavigation():JSX.Element {
 
   const filterClickHandler = (filter:string) => dispatch(changeFilter(filter));
   const setFilterClassName = (filter: string) => `main-navigation__item ${currentAppFilter === filter ? 'main-navigation__item--active' : ''}`;
-  const getFilterCount = (filter:string):number => {
-    switch (filter) {
-      case AppFilter.WatchList:
-        return moviesList.filter((movie) => movie.userDetails.watchlist).length;
-      case AppFilter.History:
-        return moviesList.filter((movie) => movie.userDetails.alreadyWatched).length;
-      case AppFilter.Favorites:
-        return moviesList.filter((movie) => movie.userDetails.favorite).length;
-      default: return moviesList.length;
-    }
-  };
 
   return (
     <div>
       <nav className="main-navigation">
         <div className="main-navigation__items">
-          <a href="#all" className={setFilterClassName(AppFilter.All)} onClick={() => filterClickHandler(AppFilter.All)}>
+          <a href="/#" className={setFilterClassName(AppFilter.All)} onClick={() => filterClickHandler(AppFilter.All)}>
             {AppFilter.All}
           </a>
-          <a href="#watchlist" className={setFilterClassName(AppFilter.WatchList)} onClick={() => filterClickHandler(AppFilter.WatchList)}>
+          <a href="/#" className={setFilterClassName(AppFilter.WatchList)} onClick={() => filterClickHandler(AppFilter.WatchList)}>
             {AppFilter.WatchList}
-            <span className="main-navigation__item-count">{getFilterCount(AppFilter.WatchList)}</span>
+            <span className="main-navigation__item-count">{getFilteredMovies(moviesList, AppFilter.WatchList).length}</span>
           </a>
-          <a href="#history" className={setFilterClassName(AppFilter.History)} onClick={() => filterClickHandler(AppFilter.History)}>
+          <a href="/#" className={setFilterClassName(AppFilter.History)} onClick={() => filterClickHandler(AppFilter.History)}>
             {AppFilter.History}
-            <span className="main-navigation__item-count">{getFilterCount(AppFilter.History)}</span>
+            <span className="main-navigation__item-count">{getFilteredMovies(moviesList, AppFilter.History).length}</span>
           </a>
-          <a href="#favorites" className={setFilterClassName(AppFilter.Favorites)} onClick={() => filterClickHandler(AppFilter.Favorites)}>
+          <a href="/#" className={setFilterClassName(AppFilter.Favorites)} onClick={() => filterClickHandler(AppFilter.Favorites)}>
             {AppFilter.Favorites}
-            <span className="main-navigation__item-count">{getFilterCount(AppFilter.Favorites)}</span>
+            <span className="main-navigation__item-count">{getFilteredMovies(moviesList, AppFilter.Favorites).length}</span>
           </a>
         </div>
-        <a href="#stats" className="main-navigation__additional">Stats</a>
+        <a href="/#" className="main-navigation__additional">Stats</a>
       </nav>
     </div>
   );

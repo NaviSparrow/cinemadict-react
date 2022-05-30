82ab48caad5cfe4416e0f1bd118ed82eaba6df42
Ivan <work.gorobec@gmail.com>
2022-05-30 20:44:25 +0500

    добавит обработку нажатия на кнопки в карточках

--- src/components/movie-card/movie-card.tsx
+++ src/components/movie-card/movie-card.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-import {MovieType} from '../../types/movie-type';
-import {BREAK_POINT_COUNT, formatRunTime, formatYear, getShortDescription} from '../../service/const';
+import {MovieType, newUserDetailsType} from '../../types/movie-type';
+import {BREAK_POINT_COUNT, FilterButton, formatRunTime, formatYear, getShortDescription} from '../../service/const';
+import {useAppDispatch} from '../../hooks/useAppDispatch';
+import {changeUserDetails} from '../../store/actions';
 
 type MovieCardProps = {
   movieCard: MovieType;
 }
 
 function MovieCard({movieCard}:MovieCardProps) {
-  const {filmInfo, comments, userDetails} = movieCard;
+  const dispatch = useAppDispatch();
+  const {filmInfo, comments, userDetails, id} = movieCard;
   const {title, totalRating, poster, description, runtime, release, genre } = filmInfo;
   const {favorite, watchlist, alreadyWatched} = userDetails;
 
   const setupDescription = ():string => description.length < BREAK_POINT_COUNT ? description : getShortDescription(description);
+
+  const cardButtonClickHandler = (newUserDetails:newUserDetailsType) => dispatch(changeUserDetails(newUserDetails));
 
   return (
     <article className="film-card">
@@ -23,16 +28,28 @@ function MovieCard({movieCard}:MovieCardProps) {
         <span className="film-card__genre">{genre[0]}</span>
       </p>
       <img src={poster} alt="poster" className="film-card__poster"/>
-      <p className="film-card__description fi">{setupDescription()}</p>
+      <p className="film-card__description">{setupDescription()}</p>
       <a href={'/#'} className="film-card__comments">{comments.length} comments</a>
       <div className="film-card__controls ">
-        <button className={`film-card__controls-item film-card__controls-item--add-to-watchlist ${watchlist ? 'film-card__controls-item--active' : ''}`} type="button">
+        <button className={`film-card__controls-item film-card__controls-item--add-to-watchlist ${watchlist ? 'film-card__controls-item--active' : ''}`} type="button"
+          onClick={() => {
+            cardButtonClickHandler({id: id, key: FilterButton.watchlist, value: !watchlist});
+          }}
+        >
           Add to watchlist
         </button>
-        <button className={`film-card__controls-item film-card__controls-item--mark-as-watched ${alreadyWatched ? 'film-card__controls-item--active' : ''}`} type="button">
+        <button className={`film-card__controls-item film-card__controls-item--mark-as-watched ${alreadyWatched ? 'film-card__controls-item--active' : ''}`} type="button"
+          onClick={() => {
+            cardButtonClickHandler({id: id, key: FilterButton.alreadyWatched, value: !alreadyWatched});
+          }}
+        >
           Mark as watched
         </button>
-        <button className={`film-card__controls-item film-card__controls-item--favorite ${favorite ? 'film-card__controls-item--active' : ''}`} type="button">
+        <button className={`film-card__controls-item film-card__controls-item--favorite ${favorite ? 'film-card__controls-item--active' : ''}`} type="button"
+          onClick={() => {
+            cardButtonClickHandler({id: id, key: FilterButton.favorite, value: !favorite});
+          }}
+        >
           Mark as favorite
         </button>
       </div>
